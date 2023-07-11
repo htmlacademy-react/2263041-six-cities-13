7a559b2dd453f3b2332ee5ed0ed6eb3df7106f42
SergeyKarpenko <honda-box@mail.ru>
2023-07-11 16:47:55 +0300

    Начинаем программировать

---
+++ src/pages/login-page/login-page.tsx
@@ -0,0 +1,36 @@
+function LoginPage(): JSX.Element {
+  return (
+    <section className="login">
+      <h1 className="login__title">Sign in</h1>
+      <form className="login__form form" action="#" method="post">
+        <div className="login__input-wrapper form__input-wrapper">
+          <label className="visually-hidden">E-mail</label>
+          <input
+            className="login__input form__input"
+            type="email"
+            name="email"
+            placeholder="Email"
+            required
+          >
+          </input>
+        </div>
+        <div className="login__input-wrapper form__input-wrapper">
+          <label className="visually-hidden">Password</label>
+          <input
+            className="login__input form__input"
+            type="password"
+            name="password"
+            placeholder="Password"
+            required
+          >
+          </input>
+        </div>
+        <button className="login__submit form__submit button" type="submit">
+          Sign in
+        </button>
+      </form>
+    </section>
+  );
+}
+
+export default LoginPage;
